e5168d8e87582313512b0bf85be0c3593023c7dc
A.Paev <ppaev@bk.ru>
2020-05-29 15:44:25 +0300

    Initial commit

---
+++ UserLists/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UserLists
+//
+//  Created by Арсений Паевщиков on 29.05.2020.
+//  Copyright © 2020 Arseniy Paevshchikov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
