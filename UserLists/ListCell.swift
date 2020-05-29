3f98c69f63b0d39b34593874dc863ae284098c82
A.Paev <ppaev@bk.ru>
2020-05-29 16:11:07 +0300

    Finished UI for UserListsTableViewController

---
+++ UserLists/ListCell.swift
@@ -0,0 +1,17 @@
+//
+//  ListCell.swift
+//  UserLists
+//
+//  Created by Арсений Паевщиков on 29.05.2020.
+//  Copyright © 2020 Arseniy Paevshchikov. All rights reserved.
+//
+
+import UIKit
+
+class ListCell: UITableViewCell {
+
+    @IBOutlet weak var creationDateLabel: UILabel!
+    @IBOutlet weak var updateDateLabel: UILabel!
+    @IBOutlet weak var textView: UITextView!
+    
+}
---
+++ UserLists/UserListsTableViewController.swift
@@ -0,0 +1,41 @@
+//
+//  UserListsTableViewController.swift
+//  UserLists
+//
+//  Created by Арсений Паевщиков on 29.05.2020.
+//  Copyright © 2020 Arseniy Paevshchikov. All rights reserved.
+//
+
+import UIKit
+
+class UserListsTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ListCell", for: indexPath)
+
+        return cell
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
